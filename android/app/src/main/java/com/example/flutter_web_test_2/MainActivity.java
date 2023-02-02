69f2dd5823e9eeea544a321d0d51a5deee34fd05
YEONSANG JEONG <wjddustkd45@gmail.com>
2023-02-02 12:58:35 +0900

    tsettest

--- android/app/src/main/java/com/example/flutter_web_test_2/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_web_test_2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.flutter_web_test_2;
 
+import android.os.Build;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
 }
+
+
